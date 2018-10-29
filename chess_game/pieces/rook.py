6059a92bda732ea59493355902f68d4317c7adb8
jrj92280 <jrj92280@gmail.com>
2018-10-29 19:00:48 -0400

    Finished pawn moves on board

--- chess_game/pieces/rook.py
+++ chess_game/pieces/rook.py
@@ -4,3 +4,9 @@ from chess_game.pieces.piece import Piece
 class Rook(Piece):
     def __init__(self, *, is_white: bool = True):
         super().__init__('r', is_white=is_white)
+
+    def hints(self, board):
+        position = self.find_piece(board)
+        hints = list()
+
+        return hints
