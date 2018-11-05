73551bd70302d60d35a218f46650bfaff49f16cc
jrj92280 <jrj92280@gmail.com>
2018-11-05 08:13:47 -0500

    Connecting backend to the frontend

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 
 from eve import Eve
-from flask import render_template, request
+from flask import render_template, request, jsonify
 
 # Heroku support: bind to PORT if defined, otherwise default to 5000.
 from chess_game._board import make_board
@@ -35,6 +35,17 @@ def chess():
     player_one = request.args.get("user") or request.form.get('user')
 
     return render_template('chess_backend.html', board=board, player_one=player_one)
+
+
+@app.route('/chess/hint', methods=['POST'])
+def chess():
+    game_board = request.args.get("game_board") or request.form.get('game_board')
+    row = request.args.get("row") or request.form.get('row')
+    column = request.args.get("column") or request.form.get('column')
+    piece = game_board[row, column].piece
+    hints = piece.hints()
+
+    return jsonify(hints)
 
 
 @app.route('/chess')
